1c27b45fe3740d06a8f03a5168c2fb21bcdbc514
Swapnil <swapnilnaique2005@gmail.com>
2023-09-08 14:36:42 +0530

    displays data in the webpage

--- Js-Project-01-RPS/script.js
+++ Js-Project-01-RPS/script.js
@@ -8,16 +8,21 @@ let usersChoice = window.prompt("Enter rock , paper or scissor",);
 //* Display computers choice as string.
 switch (computersChoice) {
     case 0:
-        console.log("Computer: rock")
+        document.write("Computer: rock<br>");
+        console.log("Computer: rock<br>")
         break;
-    case 1: console.log("Computer: paper")
+    case 1: console.log("Computer: paper<br>")
+        document.write("Computer: paper<br>");
         break;
-    case 2: console.log("Computer: scissors")
+    case 2: console.log("Computer: scissors<br>")
+        document.write("Computer: scissors<br>");
         break;
 }
 
 //* Display users choice.
-console.log("User: "+usersChoice)
+console.log("User: " + usersChoice)
+document.write("User: " + usersChoice+"<br>");
+
 
 //* Convert users choice to number.
 usersChoice = usersChoice.toLowerCase()
@@ -25,27 +30,43 @@ if (usersChoice == "rock") {
     usersChoice = 0
 } else if (usersChoice == "paper") {
     usersChoice = 1
-} else {
+} else if (usersChoice == "paper") {
     usersChoice = 2
+} else {
+    document.write("Error: Wrong input ");
 }
 
 //* check for tie
 if (computersChoice == usersChoice) {
     console.log("Its a Tie");
+    document.write("Its a Tie");
+
 }
 else {//*
     if (computersChoice == 0 && usersChoice == 1) {
         console.log("User wins")
+        document.write("User wins");
+
     } else if (computersChoice == 0 && usersChoice == 2) {
         console.log("Computer Wins")
+        document.write("Computer Wins");
+
     } else if (computersChoice == 1 && usersChoice == 0) {
         console.log("Computer Wins")
+        document.write("Computer Wins");
+
     } else if (computersChoice == 1 && usersChoice == 2) {
         console.log("User wins")
+        document.write("User wins");
+
     } else if (computersChoice == 2 && usersChoice == 0) {
         console.log("User wins")
+        document.write("User wins");
+
     } else if (computersChoice == 2 && usersChoice == 1) {
         console.log("Computer Wins")
+        document.write("Computer Wins");
+
     }
 }
 
