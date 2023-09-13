9f34c409cb1b30bf003c310330bf42e46d832561
Swapnil <swapnilnaique2005@gmail.com>
2023-09-13 17:30:42 +0530

    Added comments to js file and README.md file

--- Js-Project-01-RPS/README.md
+++ Js-Project-01-RPS/README.md
@@ -1,8 +1,109 @@
-# Rock Paper scissor
-### Rock paper Scissor game using javascript 
-<img src='ui-page.png'>
+# Rock Paper Scissor
+## About the game
+ Rock paper Scissor game using javascript 
 
+## Start of the game
+
+
+
+## What it does
 * The game prompts the user to select an option 
 * The game picks a random value for the computer 
 * The game checks if it is a Tie
-* If not, both the values are compared to find the winner
+* If not, both the values are compared to find the winner and calculate the score
+* Then, the winner and the score is displayed 
+
+
+## Working
+```javascript
+//? Event listeners for the three buttons 
+optionsBtn0.addEventListener('click', () => {
+    console.log(computer.textContent) //* displays computers choices in the console
+    userChoice.textContent = optionsBtn0.textContent //*updates users choice
+    user = 0 //*stores users choice as number for calculation
+    computersChoice() //* genrates and displayes the computers choice 
+    result() //* checks for tie and calculates the result
+    //* updates the score for the user and the computer
+    resultComp.textContent = computerScore 
+    resultuser.textContent = userScore
+})
+optionsBtn1.addEventListener('click', () => {
+    console.log(computer.textContent)
+    userChoice.textContent = optionsBtn1.textContent
+    user = 1
+    computersChoice()
+    result()
+    resultComp.textContent = " "+computerScore
+    resultuser.textContent = " "+userScore
+})
+optionsBtn2.addEventListener('click', () => {
+    console.log(computer.textContent)
+    userChoice.textContent = optionsBtn2.textContent
+    user = 2
+    computersChoice()
+    result()
+    resultComp.textContent = " "+computerScore
+    resultuser.textContent = " "+userScore
+})
+
+```
+
+The three user input buttons have event listeners which listen to _clicks_ by the _user_. The user can select from Rock paper or Scissor. Internally the user input are saved as number between 0-2.
+
+```javascript
+function computersChoice() {
+    //*Generate computers choice 
+    computerChoice = Math.floor(Math.random() * 3) //* generates a number between 0-2 
+    switch (computerChoice) {
+        case 0:
+            computer.textContent = optionsBtn0.textContent //* Updates the computers choice based on the generated number
+            //* optionsBtn0.textContent ="✊🏻"
+            break; 
+        case 1:
+            computer.textContent = optionsBtn1.textContent
+            //* optionsBtn1.textContent ="✋🏻"
+            break;
+        case 2:
+            computer.textContent = optionsBtn2.textContent
+            //* optionsBtn2.textContent ="✌🏻"
+            break;
+    }
+}
+```
+ The computer also generates a number between 0-2.
+
+```javascript
+function result() {
+    //* check for tie
+    if (computerChoice == user) {
+        header.textContent = "Its a Tie" //* updates the header to display the winner
+    }
+    else {//* if there is no tie the winner is calculated
+        if (computerChoice == 0 && user == 1) {
+            header.textContent = "User wins" //* updates the header to display the winner
+            userScore = userScore + 1 //* increments user score
+
+        } else if (computerChoice == 0 && user == 2) {
+            header.textContent = "Computer Wins"
+            computerScore = computerScore + 1 //* increments computers score
+
+        } else if (computerChoice == 1 && user == 0) {
+            header.textContent = "Computer Wins"
+            computerScore = computerScore + 1 //* increments computers score
+
+        } else if (computerChoice == 1 && user == 2) {
+            header.textContent = "User wins"
+            userScore = userScore + 1 //* increments user score
+
+        } else if (computerChoice == 2 && user == 0) {
+            header.textContent = "User wins"
+            userScore = userScore + 1 //* increments user score
+
+        } else if (computerChoice == 2 && user == 1) {
+            header.textContent = "Computer Wins"
+            computerScore = computerScore + 1 //* increments computers score
+        }
+    }
+}
+```
+The user and computer's input are saved as numbers and compared to check for the winner using the above function. It also increments the winners score and displays who won. 
--- Js-Project-01-RPS/ui.js
+++ Js-Project-01-RPS/ui.js
@@ -1,62 +1,64 @@
-let computerChoice
-let computer = document.getElementById('computer')
-let user = 0
-let header = document.getElementById('header')
-let resultComp = document.getElementById('compw')
+
+//? variables
+let computerChoice //* stores the computers choice (number)
+let user = 0 //*stores the input as number
+let computerScore = 0 //* stores the users score
+let userScore = 0 //* stores the computers score
+
+let computer = document.getElementById('computer') //* selects the element that displays the computers choice
+let userChoice = document.getElementById('user') //* selects the element that displays the computers choice
+
+//* selecting the elements that show the score
 let resultuser = document.getElementById('userw')
+let resultComp = document.getElementById('compw')
 
-// resultComp.innerHTML='hello'
-
-let computerScore = 0
-let userScore = 0
-
+//* selecting the three user input buttons
 let optionsBtn0 = document.getElementById('options-btn-0')
 let optionsBtn1 = document.getElementById('options-btn-1')
 let optionsBtn2 = document.getElementById('options-btn-2')
+
+//* selects element to display the winner 
+let header = document.getElementById('header')
+
+//* storing the width and height of the buttons 
 let boxWidth = optionsBtn0.offsetWidth + "px"
 let boxHeight = optionsBtn0.offsetHeight + "px"
-console.log(boxWidth)
-console.log(boxHeight)
 
-let userChoice = document.getElementById('user')
+//* applying height and width to the elements where computers and users choice is displayed
 userChoice.style.width = boxWidth
 userChoice.style.height = boxHeight
-
-
 computer.style.width = boxWidth
 computer.style.height = boxHeight
 
 
-optionsBtn0.addEventListener('click', (e) => {
-    console.log(computer.textContent)
-    userChoice.textContent = optionsBtn0.textContent
-    user = 0
-    // console.log(user)
-    computersChoice()
-    result()
-    // scoreUpdate()
-    resultComp.textContent = " "+computerScore
-    resultuser.textContent = " "+userScore
+
+
+//? Event listeners for the three buttons 
+optionsBtn0.addEventListener('click', () => {
+    console.log(computer.textContent) //* displays computers choices in the console
+    userChoice.textContent = optionsBtn0.textContent //*updates users choice
+    user = 0 //*stores users choice as number for calculation
+    computersChoice() //* generates and displays the computers choice 
+    result() //* checks for tie and calculates the result
+    //* updates the score for the user and the computer
+    resultComp.textContent = computerScore 
+    resultuser.textContent = userScore
 })
-optionsBtn1.addEventListener('click', (e) => {
+optionsBtn1.addEventListener('click', () => {
     console.log(computer.textContent)
     userChoice.textContent = optionsBtn1.textContent
     user = 1
-    // console.log(user)
     computersChoice()
     result()
-    // scoreUpdate()
     resultComp.textContent = " "+computerScore
     resultuser.textContent = " "+userScore
 })
-optionsBtn2.addEventListener('click', (e) => {
+optionsBtn2.addEventListener('click', () => {
     console.log(computer.textContent)
     userChoice.textContent = optionsBtn2.textContent
     user = 2
-    // console.log(user)
     computersChoice()
     result()
-    // scoreUpdate()
     resultComp.textContent = " "+computerScore
     resultuser.textContent = " "+userScore
 
@@ -64,95 +66,59 @@ optionsBtn2.addEventListener('click', (e) => {
 
 
 
+
+//? Functions
 function result() {
     //* check for tie
     if (computerChoice == user) {
-        header.textContent = "Its a Tie"
-        // userScore = userScore + 1
-        // computerScore = computerScore + 1
-        console.log("user score " + userScore)
-        console.log("computer score " + userScore)
+        header.textContent = "Its a Tie" //* updates the header to display the winner
     }
-    else {//*
+    else {//* if there is no tie the winner is calculated
         if (computerChoice == 0 && user == 1) {
-            header.textContent = "User wins"
-            userScore = userScore + 1
-            console.log("user score " + userScore)
-            console.log("computer score " + computerScore)
-            // resultComp.textContent = computerScore
-            // resultuser.textContent = userScore
+            header.textContent = "User wins" //* updates the header to display the winner
+            userScore = userScore + 1 //* increments user score
 
         } else if (computerChoice == 0 && user == 2) {
             header.textContent = "Computer Wins"
-            computerScore = computerScore + 1
-
-            console.log("user score " + userScore)
-            console.log("computer score " + computerScore)
-            // resultComp.textContent = computerScore
-            // resultuser.textContent = userScore
+            computerScore = computerScore + 1 //* increments computers score
 
         } else if (computerChoice == 1 && user == 0) {
             header.textContent = "Computer Wins"
             computerScore = computerScore + 1
 
-            // resultComp.textContent = computerScore
-            // resultuser.textContent = userScore
-            console.log("user score " + userScore)
-            console.log("computer score " + computerScore)
-
         } else if (computerChoice == 1 && user == 2) {
             header.textContent = "User wins"
             userScore = userScore + 1
-            // resultComp.textContent = computerScore
-            // resultuser.textContent = userScore
-            console.log("user score " + userScore)
-            console.log("computer score " + computerScore)
-
 
         } else if (computerChoice == 2 && user == 0) {
             header.textContent = "User wins"
             userScore = userScore + 1
-            // resultComp.textContent = computerScore
-            // resultuser.textContent = userScore
-            console.log("user score " + userScore)
-            console.log("computer score " + computerScore)
-
 
         } else if (computerChoice == 2 && user == 1) {
             header.textContent = "Computer Wins"
             computerScore = computerScore + 1
-            resultComp.textContent = computerScore
-            resultuser.textContent = userScore
-            console.log("user score " + userScore)
-            console.log("computer score " + userScore)
 
         }
     }
 }
 
-function computerReset() {
-    // * to reset the computer choice box 
-    computer.innerHTML = "&nbsp;"
-}
-
 function computersChoice() {
 
-    //*Genrate computers choice 
-    computerChoice = Math.floor(Math.random() * 3)
+    //*Generate computers choice 
+    computerChoice = Math.floor(Math.random() * 3) //* generates a number between 0-2 
     switch (computerChoice) {
         case 0:
-            computer.textContent = optionsBtn0.textContent
-            console.log("Computer: rock")
-            break;
+            computer.textContent = optionsBtn0.textContent //* Updates the computers choice based on the generated number
+            //* optionsBtn0.textContent ="✊🏻"
+            break; 
         case 1:
             computer.textContent = optionsBtn1.textContent
-            console.log("Computer: paper")
+            //* optionsBtn1.textContent ="✋🏻"
             break;
         case 2:
             computer.textContent = optionsBtn2.textContent
-            console.log("Computer: scissors")
+            //* optionsBtn2.textContent ="✌🏻"
             break;
-
     }
 }
 
