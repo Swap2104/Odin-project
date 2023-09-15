0f2d392c96a39ed9c6cda6610f1002953f56c863
Swapnil <swapnilnaique2005@gmail.com>
2023-09-15 15:13:30 +0530

    Added clear, erase, normal button and color picker option

--- Etch-a-sketch/script.js
+++ Etch-a-sketch/script.js
@@ -1,10 +1,10 @@
-
+let mouseColor='black'
 let gridBox = document.getElementsByClassName('gridBox')
 console.log(gridBox.length)
 for (let i = 0; i < gridBox.length; i++) {
     // gridBox[i].style.backgroundColor = "red";
     gridBox[i].addEventListener('mouseover', (e) => {
-        gridBox[i].style.background = "black";
+        gridBox[i].style.background = mouseColor;
         // div.style.background = "red";
         console.log(e.target)
         console.log(e.type)
@@ -12,5 +12,25 @@ for (let i = 0; i < gridBox.length; i++) {
 }
 
 
+let clear = document.getElementById('clear')
+clear.addEventListener('click', () => {
+    for (let i = 0; i < gridBox.length; i++) {
+        gridBox[i].style.background = "white";
+    }
+})
 
 
+let eraser= document.getElementById('eraser')
+eraser.addEventListener('click', ()=>{
+    mouseColor="white"
+})
+
+let normal= document.getElementById('normal')
+normal.addEventListener('click', ()=>{
+    mouseColor="black"
+})
+
+let colorPicker=document.getElementById('colorPicker')
+colorPicker.addEventListener('change', ()=>{
+    mouseColor=colorPicker.value
+})
