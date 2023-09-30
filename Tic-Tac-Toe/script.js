81dc63c68ff6d709247564f5c1dd0d0fd3abc54d
Swapnil <swapnilnaique2005@gmail.com>
2023-09-30 17:30:48 +0530

    User input and computer input added

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -1,25 +1,36 @@
-let gameBoard=[]
-
-gameBoard=["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+let gameBoard = [];
 
 const game = (() => {
-
-    const gameDisplayController = () =>{
-        let squares=document.getElementsByClassName('squares')
+    const squares = document.getElementsByClassName('squares');
+    const gameDisplayController = () => {
         for (let i = 0; i < squares.length; i++) {
-                // console.log(gameBoard[i], squares[i].textContent)
-                squares[i].textContent=gameBoard[i]
-                    squares[i].addEventListener('click', ()=>{
-                        squares[i]
-                    })
-            }    
-    } 
+            squares[i].textContent = gameBoard[i];
+        }
+    }
+
+    const userInput = () => {
+        for (let i = 0; i < squares.length; i++) {
+            squares[i].addEventListener('click', () => {
+                console.log("hello from userInput");
+                gameBoard[squares[i].id] = "X";
+                game.gameDisplayController();
+                game.computerInput()
+            });
+        }
+    }
+
+    const computerInput = () => {
+        let compRand = Math.floor(Math.random() * 8);
+        gameBoard[compRand] = "O";
+        game.gameDisplayController();
+
+    }
+
     return {
-        gameDisplayController
+        gameDisplayController,
+        userInput,
+        computerInput
     };
-  })();
+})();
 
-
-
-
-
+game.userInput();
