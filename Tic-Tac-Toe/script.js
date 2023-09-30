dc475c49631f58542e9c4c04c2b6fcff60dae886
Swapnil <swapnilnaique2005@gmail.com>
2023-09-30 21:42:19 +0530

    made inputs uneditable

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -1,29 +1,37 @@
-let gameBoard = [];
+
 
 const game = (() => {
     const squares = document.getElementsByClassName('squares');
     const gameDisplayController = () => {
         for (let i = 0; i < squares.length; i++) {
             squares[i].textContent = gameBoard[i];
+            // console.log(gameBoard[i])
+            // console.log(gameBoard[10])
         }
     }
 
     const userInput = () => {
         for (let i = 0; i < squares.length; i++) {
             squares[i].addEventListener('click', () => {
-                console.log("hello from userInput");
-                gameBoard[squares[i].id] = "X";
-                game.gameDisplayController();
-                game.computerInput()
+                // console.log("hello from userInput");
+                const squareIndex = parseInt(squares[i].id);
+                if (gameBoard[squareIndex] === "") {
+                    gameBoard[squareIndex] = "X";
+                    game.gameDisplayController();
+                    game.computerInput();
+                }
             });
         }
     }
 
     const computerInput = () => {
-        let compRand = Math.floor(Math.random() * 8);
+        let compRand;
+        do {
+            compRand = Math.floor(Math.random() * 9);
+        } while (gameBoard[compRand] !== "");
+
         gameBoard[compRand] = "O";
         game.gameDisplayController();
-
     }
 
     return {
@@ -33,4 +41,6 @@ const game = (() => {
     };
 })();
 
+let gameBoard = ["", "", "", "", "", "", "", "", ""];
+game.gameDisplayController()
 game.userInput();
