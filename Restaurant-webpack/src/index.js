91866bcc6d38a33339c05d7033433f6b848b89ea
Swapnil <swapnilnaique2005@gmail.com>
2023-10-05 00:33:53 +0530

    added navbar logic in index.html

--- Restaurant-webpack/src/index.js
+++ Restaurant-webpack/src/index.js
@@ -1,10 +1,21 @@
 import { initialLoad } from "./initial-load";
 import { home } from "./home";
-// let home =document.getElementById('home')
-// let content=document.getElementById('content')
+
 initialLoad()
-// home.addEventListener('click', ()=>{
-    // console.log(content.innerHTML)
+home()
+
+let content=document.getElementById('content')
+let home_nav=document.getElementById('home')
+let menu_nav=document.getElementById('menu')
+let contact_nav=document.getElementById('contact')
+home_nav.addEventListener('click', ()=>{
     home()
-// })
-// home()
+    // content.innerHTML=""
+    // console.log(content.innerHTML)
+})
+menu_nav.addEventListener('click', ()=>{
+    home()
+})
+contact_nav.addEventListener('click', ()=>{
+    home()
+})
