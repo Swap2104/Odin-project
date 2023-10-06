cb073c95bc3bca7e36112944e5d0b71faa0a4beb
Swapnil <swapnilnaique2005@gmail.com>
2023-10-06 11:06:20 +0530

    added initial Load styling

--- to-do-list/src/topbar.js
+++ to-do-list/src/topbar.js
@@ -1,7 +1,11 @@
 function topBar(){
     let content=document.getElementById('content')
     let topBar =document.createElement('div')
+    let topBar_header =document.createElement('h1')
+    topBar_header.className="top-bar-head"
+    topBar_header.textContent="To-Do list"
     topBar.id="top-bar"
+    topBar.appendChild(topBar_header)
     content.appendChild(topBar)
 }
 export {topBar}
