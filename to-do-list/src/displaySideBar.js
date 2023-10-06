53a10b40bfbc6b1f7a9f30c70f0869c13eaaf184
Swapnil <swapnilnaique2005@gmail.com>
2023-10-06 13:17:33 +0530

    displayed project list

--- to-do-list/src/displaySideBar.js
+++ to-do-list/src/displaySideBar.js
@@ -1,8 +1,10 @@
 function displaySideBar(){
-    let side_bar=document.getElementById('side-bar')
     let task = document.getElementById('tasks')
+    let project = document.getElementById('projects')
     let tasks_list = JSON.parse(localStorage.getItem("projects"))[0].tasks
+    let projects_list = JSON.parse(localStorage.getItem("projects"))
     console.log(tasks_list)
+    console.log(projects_list[2].name)
     // console.log(tasks_list[0].title)
     let list = document.createElement('ul');
     for (let i = 0; i < tasks_list.length; ++i) {
@@ -11,6 +13,15 @@ function displaySideBar(){
         list.appendChild(li);
     }
 
+    let list_project = document.createElement('ul');
+    for (let i = 1; i < projects_list.length; ++i) {
+        let li = document.createElement('li');
+        li.textContent = projects_list[i].name;
+        console.log(tasks_list[i].name)
+        list_project.appendChild(li);
+    }
+
     task.appendChild(list)
+    project.appendChild(list_project)
 }
 export {displaySideBar}
