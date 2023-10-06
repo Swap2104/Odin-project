fad2576191ac0d856684ae88a77ff9a3d8cdf302
Swapnil <swapnilnaique2005@gmail.com>
2023-10-06 13:28:42 +0530

    added add item button

--- to-do-list/src/main.js
+++ to-do-list/src/main.js
@@ -1,7 +1,11 @@
 function main(){
     let content=document.getElementById('content')
     let main =document.createElement('div')
+    let btn =document.createElement('button')
+    btn.className="btn"
+    btn.textContent="Add Tasks"
     main.id="main"
     content.appendChild(main)
+    main.appendChild(btn)
 }
 export {main}
