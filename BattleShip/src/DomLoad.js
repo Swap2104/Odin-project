e30920ddbbf94a8dca790f479621e93288ec0ca8
Swapnil <swapnilnaique2005@gmail.com>
2023-10-17 11:21:52 +0530

    created grid for ship placement

--- BattleShip/src/DomLoad.js
+++ BattleShip/src/DomLoad.js
@@ -1,3 +1,16 @@
-// ship placement
-let content=document.getElementById('content')
-content.textContent="hello world"
+//* ship placement
+function ship_placement(){
+    let content = document.getElementById('content')
+    let placement = document.createElement('div')
+    placement.id = "placement"
+    content.appendChild(placement)
+    // placement.textContent = "hello world"
+    //* creating grid
+    for (let i = 0; i < 100; i++) {
+        let div=document.createElement('div')        
+        placement.appendChild(div)
+        div.className="div"
+    }
+}
+
+ship_placement()
