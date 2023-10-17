d2e4b85ba4ab5c7b27c8fa4844f0c244c9b3d010
Swapnil <swapnilnaique2005@gmail.com>
2023-10-17 11:36:48 +0530

    added co-ordinates for grid

--- BattleShip/src/DomLoad.js
+++ BattleShip/src/DomLoad.js
@@ -1,16 +1,22 @@
 //* ship placement
-function ship_placement(){
+function ship_placement() {
     let content = document.getElementById('content')
     let placement = document.createElement('div')
     placement.id = "placement"
     content.appendChild(placement)
-    // placement.textContent = "hello world"
     //* creating grid
-    for (let i = 0; i < 100; i++) {
-        let div=document.createElement('div')        
+    for (let i = 0, j = 0; i < 100; i++) {
+        let div = document.createElement('div')
         placement.appendChild(div)
-        div.className="div"
+        div.className = "div"
+        //* assigning id as co-ordinates to divs
+        div.id = `${[i]},${[j]}`
+        j++
+        div.addEventListener('click', () => {
+            console.log(div.id)
+        })
     }
+
 }
 
 ship_placement()
