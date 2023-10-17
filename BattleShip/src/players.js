08dc611b51183728bb44a6df8bc9f5e6ca668daf
Swapnil <swapnilnaique2005@gmail.com>
2023-10-17 10:41:40 +0530

    added computer's input

---
+++ BattleShip/src/players.js
@@ -0,0 +1,15 @@
+import gameBoard from "./gameBoard.js"
+const player = new gameBoard()
+
+
+function comp() {
+    let compX = Math.floor(Math.random() * 10)
+    console.log(compX)
+    let compY = Math.floor(Math.random() * 10)
+    console.log(compY)
+    player.attack([compX, compY])
+    // player.attack([compX, compY])    
+}
+
+comp()
+exports=comp
