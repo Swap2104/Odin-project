bae3f5675a4d2b7f9b6c93266dcbcb51c3e4dcf8
Swapnil <swapnilnaique2005@gmail.com>
2023-10-23 15:07:15 +0530

    made computer's input unique

--- battleship-2/src/computer.js
+++ battleship-2/src/computer.js
@@ -7,24 +7,28 @@ function comp() {
 
 //? function to calculate computer's ship placement
 function compShipPlacement() {
-    let comp_ship_array = []
-    let compBoatCoOrdinates = Math.floor(Math.random() * 100)
-    comp_ship_array.push(new Ship("boat", 2, compBoatCoOrdinates, Math.floor(Math.random() * 2) < 0.5))
+    let comp_ship_array = [];
+    let usedCoordinates = new Set();
 
-    let compSubmarineCoOrdinates = Math.floor(Math.random() * 100)
-    comp_ship_array.push(new Ship("Submarine", 3, compSubmarineCoOrdinates, Math.floor(Math.random() * 2) < 0.5))
+    function generateUniqueCoordinate(length) {
+        let coordinate;
+        do {
+            coordinate = Math.floor(Math.random() * 100);
+        } while (usedCoordinates.has(coordinate) || usedCoordinates.has(coordinate + length - 1));
 
-    let compBattleshipCoOrdinates = Math.floor(Math.random() * 100)
-    comp_ship_array.push(new Ship("Battleship", 5, compBattleshipCoOrdinates, Math.floor(Math.random() * 2) < 0.5))
+        for (let i = coordinate; i < coordinate + length; i++) {
+            usedCoordinates.add(i);
+        }
 
-    let compCarrierCoOrdinates = Math.floor(Math.random() * 100)
-    comp_ship_array.push(new Ship("Carrier", 7, compCarrierCoOrdinates, Math.floor(Math.random() * 2) < 0.5))
+        return coordinate;
+    }
 
-    //! Temp
-    // console.log(comp_ship_array)
+    comp_ship_array.push(new Ship("Boat", 2, generateUniqueCoordinate(2), Math.random() < 0.5));
+    comp_ship_array.push(new Ship("Submarine", 3, generateUniqueCoordinate(3), Math.random() < 0.5));
+    comp_ship_array.push(new Ship("Battleship", 5, generateUniqueCoordinate(5), Math.random() < 0.5));
+    comp_ship_array.push(new Ship("Carrier", 7, generateUniqueCoordinate(7), Math.random() < 0.5));
 
-    return comp_ship_array
+    return comp_ship_array;
 }
-
-export default comp 
-export {compShipPlacement}
+export default comp
+export { compShipPlacement }
