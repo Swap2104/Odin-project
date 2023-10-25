e7f3ce0abb7dc91c373fe660508ce17499e9ac5e
Swapnil <swapnilnaique2005@gmail.com>
2023-10-25 15:11:00 +0530

    added function to calculate all the position of computer's ship

--- battleship-2/src/computer.js
+++ battleship-2/src/computer.js
@@ -6,29 +6,81 @@ function comp() {
 }
 
 //? function to calculate computer's ship placement
-function compShipPlacement() {
-    let comp_ship_array = [];
-    let usedCoordinates = new Set();
+function compShipPlacement(ship_length, ship_number) {
+    let ship_placement = [] //? to stores the orientation and  unique starting co ordinate for each ship
+    let comp_ship_array = []; //? The array that stores all the ship objects.
+    let usedCoordinates = [] //? array to store used Co-Ordinates
+    let position = [] //? array to store all the positions
 
-    function generateUniqueCoordinate(length) {
-        let coordinate;
-        do {
-            coordinate = Math.floor(Math.random() * 100);
-        } while (usedCoordinates.has(coordinate) || usedCoordinates.has(coordinate + length - 1));
+    //? function to generate a unique random number for the ship, So that they do not overlap.
 
-        for (let i = coordinate; i < coordinate + length; i++) {
-            usedCoordinates.add(i);
+    function uniqueCoOrdinates(length, i) {
+        let endCo
+        //? `i` is used to count the number of the ship
+        let dir = Math.random() < 0.5; //? generates true or false for the orientation.
+        while (usedCoordinates.length < i) {
+            var r = Math.floor(Math.random() * 100);
+            if (usedCoordinates.indexOf(r) === -1) usedCoordinates.push(r);
+        }
+        let startCo = usedCoordinates[i - 1] //? initializing the start co-ordinate 
+
+        //? calculating the end co-ordinate used to calculate all the position occupied by the ship.
+        if (dir) {
+            for (let j = 0; j < length; j++) {
+                endCo = startCo++ //? `i-1` because we count i from 1
+            }
+        }
+        if (!dir) {
+            endCo = startCo
+            for (let j = 1; j < length; j++) {
+                endCo += 10
+            }
         }
 
-        return coordinate;
+        // ? calculating all the position occupied by the ship. 
+        if (dir) {
+            for (let k = usedCoordinates[i - 1]; k <= endCo; k++) {
+                position.push(k);
+            }
+        }
+        if (!dir) {
+            for (let k = startCo; k <= endCo; k += 10) {
+                position.push(k);
+            }
+        }
+
+        return { position, dir }
     }
 
-    comp_ship_array.push(new Ship("Boat", 2, generateUniqueCoordinate(2), Math.random() < 0.5));
-    comp_ship_array.push(new Ship("Submarine", 3, generateUniqueCoordinate(3), Math.random() < 0.5));
-    comp_ship_array.push(new Ship("Battleship", 5, generateUniqueCoordinate(5), Math.random() < 0.5));
-    comp_ship_array.push(new Ship("Carrier", 7, generateUniqueCoordinate(7), Math.random() < 0.5));
+    let ship_object = uniqueCoOrdinates(ship_length, ship_number)
 
-    return comp_ship_array;
+    return ship_object;
 }
+
+
+// ? this function creates the ship objects
+function AIShips() {
+    let b = [2, 3, 5, 7]
+    let a = [1, 2, 3, 4]
+    let comp_ship_array = []
+
+    let ship_object = compShipPlacement(b[0], a[0])
+    comp_ship_array.push(new Ship("Boat", b[0], ship_object.position[0], ship_object.dir));
+    console.log(comp_ship_array)
+
+    ship_object = compShipPlacement(b[1], a[1])
+    comp_ship_array.push(new Ship("Submarine", b[1], ship_object.position[0], ship_object.dir));
+    console.log(comp_ship_array)
+
+    ship_object = compShipPlacement(b[2], a[2])
+    comp_ship_array.push(new Ship("Battleship", b[2], ship_object.position[0], ship_object.dir));
+    console.log(comp_ship_array)
+
+    ship_object = compShipPlacement(b[3], a[3])
+    comp_ship_array.push(new Ship("Carrier", b[3], ship_object.position[0], ship_object.dir));
+    console.log(comp_ship_array)    
+    return comp_ship_array
+}
+
 export default comp
-export { compShipPlacement }
+export { AIShips }
--- battleship-2/src/game.js
+++ battleship-2/src/game.js
@@ -2,7 +2,7 @@ let start = Date.now();
 // import { objects } from "./index.js"
 import { objects } from "./Ship.js"
 import comp from "./computer.js";
-import { compShipPlacement } from "./computer.js";
+import { AIShips } from "./computer.js";
 
 let player_grid = document.getElementById('player-grid')
 let comp_grid = document.getElementById('comp-grid')
@@ -20,21 +20,7 @@ for (let i = 0; i < 100; i++) {
 let objects_arr = JSON.parse(localStorage.getItem("data"))
 let player_div_color = document.getElementsByClassName('player-div')
 
-//* working
-//! 975 millisecond
-// for (let k = 0; k < player_div_color.length; k++) {
-    //     for (let i = 0; i < objects_arr.length; i++) {
-        //         for (let j = 0; j < objects_arr[i].position.length; j++) {
-            //             console.log(Number(player_div_color[k].id))
-            //             console.log(objects_arr[i].position[j])
-            //             if (Number(player_div_color[k].id) == objects_arr[i].position[j]) {
-                //                 player_div_color[k].classList.add("ship")
-                //                 break
-                //             }
-                //         }
-                //     }
-                // }
-                
+
 //! 19 millisecond
 for (let i = 0; i < objects_arr.length; i++) {
     for (let j = 0; j < objects_arr[i].position.length; j++) {
@@ -49,23 +35,33 @@ for (let i = 0; i < objects_arr.length; i++) {
     }
 }
 
-let computer=compShipPlacement()
+// ? the `compShipPlacement` function generates all the objects for the ship and returns an array of all the objects.
+let computer = AIShips()
 let comp_div_color = document.getElementsByClassName('comp-div')
+let comp_ship
 console.log(computer)
 
-
-for (let i = 0; i < computer.length; i++) {
-    for (let j = 0; j < computer[i].position.length; j++) {
-        //? console.log(computer[i].position[j])
-        for (let k = 0; k < player_div_color.length; k++) {
-        //     //? console.log(Number(comp_div_color[k].id))
-            if (Number(player_div_color[k].id) == computer[i].position[j]) {
-                comp_div_color[k].classList.add("ship")
-                break
+//? this function displays computer's ship 
+function Comp_ship() {
+    for (let i = 0; i < computer.length; i++) {
+        for (let j = 0; j < computer[i].position.length; j++) {
+            //? console.log(computer[i].position[j])
+            for (let k = 0; k < player_div_color.length; k++) {
+                //     //? console.log(Number(comp_div_color[k].id))
+                if (Number(player_div_color[k].id) == computer[i].position[j]) {
+                    comp_div_color[k].classList.add("comp-ship")
+                    break
+                }
             }
         }
     }
+    // ? selecting the divs where computer has placed a ship. The total number of divs must be 17
+    comp_ship = document.getElementsByClassName('comp-ship')
 }
+
+do {
+    Comp_ship()
+} while (comp_ship.length > 17);
 
 let timeTaken = Date.now() - start;
 console.log("Total time taken : " + timeTaken + " milliseconds");
