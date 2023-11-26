d210cb1fdd6a176ce137c09f6f84c258dfc24176
Swapnil <108745622+Swapnilnaique@users.noreply.github.com>
2023-11-26 21:12:03 +0530

    added thumbnail for each project

--- github_pages/main.js
+++ github_pages/main.js
@@ -1,5 +1,7 @@
 import { Navbar } from "./navabar.js"
 import { SignUP } from "./project_data.js"
+import { odin_recipes } from "./project_data.js"
+
 
 Navbar()
 
@@ -9,5 +11,7 @@ let JSON_var = `[
         "Desc":"This is my restaurant page project built using webpack"
     ]`
 
-console.log(typeof(SignUP))
-console.log(SignUP)
+let img = document.getElementById('img')
+// img.style.backgroundImage = url(odin_recipes.img)
+
+console.log(odin_recipes.img)
--- github_pages/project_data.js
+++ github_pages/project_data.js
@@ -5,15 +5,74 @@
 //? Img:"path to the project thumbnail"
 //? }
 
+export const odin_recipes = {
+    name: "odin-recipes",
+    desc: "A very simple recipes page only using html. It is the first project in the odin-project curriculum",
+    img: "../odin_recipes/thumbnail.png"
+}
 
-export const SignUP= {
-        name: "Sign up page",
-        desc: "Simple sign-up page with Password validation built using HTML, CSS and javascript.",
-        img:""
-    }
+export const Dashboard = {
+    name: "Dashboard",
+    desc: "Simple dashboard template",
+    img: ""
+}
 
-export const odin_recipes= {
-        name: "odin-recipes",
-        desc: "A very simple recipes page only using html. It is the first project in the odin-project curriculum",
-        img:""
-    }
+export const calculator = {
+    name: "Calculator",
+    desc: "Web based calculator project that performs simple arithmetic operations",
+    img: ""
+}
+
+export const Eas = {
+    name: "Etch-a-sketch",
+    desc: "Etch-a-sketch webpage built using javascript",
+    img: ""
+}
+
+export const SignUP = {
+    name: "Sign up page",
+    desc: "Simple sign-up page with Password validation built using HTML, CSS and javascript",
+    img: ""
+}
+
+export const rps = {
+    name: "Library",
+    desc: "Web based Rock paper Scissor game",
+    img: ""
+}
+
+export const Library = {
+    name: "Library",
+    desc: "Web based personal library which allows the user to store the names of the books they have read and want to read",
+    img: ""
+}
+
+export const TDL = {
+    name: "To-Do List",
+    desc: "Web based To-Do list to monitor your daily tasks",
+    img: ""
+}
+
+export const TicTacToe = {
+    name: "Tic Tac Toe",
+    desc: "Play Tic-Tac-Toe with AI",
+    img: ""
+}
+
+export const Weather_app = {
+    name: "Weather App",
+    desc: "Web app to check weather around the world. This app was built using webpack",
+    img: ""
+}
+
+export const Restaurant = {
+    name: "Restaurant Page",
+    desc: "Working Restaurant page built using webpack",
+    img: ""
+}
+
+export const battleship = {
+    name: "Library",
+    desc: "Battleship game, which allows the user to play battleship in their browser",
+    img: ""
+}
