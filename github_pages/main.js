bfadad6385b3d097ffd1b1a7d0eb14d3f9666344
Swapnil <108745622+Swapnilnaique@users.noreply.github.com>
2023-11-27 01:12:46 +0530

    added recent projects sections

--- github_pages/main.js
+++ github_pages/main.js
@@ -1,17 +1,39 @@
 import { Navbar } from "./navabar.js"
-import { SignUP } from "./project_data.js"
-import { odin_recipes } from "./project_data.js"
+// ! not needed any more
+// import { odin_recipes } from "./project_data.js"
+// ? import project data
+import { data } from "./project_data.js"
 
+//? drawing navbar on the screen
 
 Navbar()
 
+let img = document.getElementsByClassName('img')
+let img_id = document.getElementById('img-id')
 
-let JSON_var = `[
-        "name": "Restaurant",
-        "Desc":"This is my restaurant page project built using webpack"
-    ]`
 
-let img = document.getElementById('img')
-// img.style.backgroundImage = url(odin_recipes.img)
+const project_0 = document.getElementById('project0')
+const project_1 = document.getElementById('project1')
+const project_2 = document.getElementById('project2')
+const project_3 = document.getElementById('project3')
+console.log(data.TicTacToe.name)
+console.log(project_0)
 
-console.log(odin_recipes.img)
+
+
+project_0.textContent = data.TicTacToe.name
+project_1.textContent = data.Weather_app.name
+project_2.textContent = data.Restaurant.name
+project_3.textContent = data.battleship.name
+
+
+
+
+// for (let i = 0; i < data.length; i++) {
+//     console.log(data[i].img)
+//     console.log({ i })
+//     console.log(img[i])
+//     img[i].style.backgroundImage = "url(" + data[i].img + ")"
+//     // img[1].style.backgroundImage = "url(" + data[1].img + ")"
+// }
+// console.log(data.length)
--- github_pages/project_data.js
+++ github_pages/project_data.js
@@ -8,71 +8,76 @@
 export const odin_recipes = {
     name: "odin-recipes",
     desc: "A very simple recipes page only using html. It is the first project in the odin-project curriculum",
-    img: "../odin_recipes/thumbnail.png"
+    img: "../odin-recipes/thumbnail.png"
 }
 
 export const Dashboard = {
     name: "Dashboard",
     desc: "Simple dashboard template",
-    img: ""
+    img: "../Dashboard/thumbnail.png"
 }
 
 export const calculator = {
     name: "Calculator",
     desc: "Web based calculator project that performs simple arithmetic operations",
-    img: ""
+    img: "../Calculator/thumbnail.png"
 }
 
 export const Eas = {
     name: "Etch-a-sketch",
     desc: "Etch-a-sketch webpage built using javascript",
-    img: ""
+    img: "../Etch-a-sketch/thumbnail.png"
 }
 
 export const SignUP = {
     name: "Sign up page",
     desc: "Simple sign-up page with Password validation built using HTML, CSS and javascript",
-    img: ""
+    img: "../sign-up/thumbnail.png"
 }
 
 export const rps = {
-    name: "Library",
+    name: "Rock Paper Scissors",
     desc: "Web based Rock paper Scissor game",
-    img: ""
+    img: "../RPS/thumbnail.png"
 }
 
 export const Library = {
     name: "Library",
     desc: "Web based personal library which allows the user to store the names of the books they have read and want to read",
-    img: ""
+    img: "../Library/thumbnail.png"
 }
 
-export const TDL = {
+export const TDList = {
     name: "To-Do List",
     desc: "Web based To-Do list to monitor your daily tasks",
-    img: ""
+    img: "../to-do-list/thumbnail.png"
 }
 
 export const TicTacToe = {
     name: "Tic Tac Toe",
     desc: "Play Tic-Tac-Toe with AI",
-    img: ""
+    img: "../Tic-Tac-Toe/thumbnail.png"
 }
 
 export const Weather_app = {
     name: "Weather App",
     desc: "Web app to check weather around the world. This app was built using webpack",
-    img: ""
+    img: "../Weather-app/thumbnail.png"
 }
 
 export const Restaurant = {
     name: "Restaurant Page",
     desc: "Working Restaurant page built using webpack",
-    img: ""
+    img: "../Restaurant-webpack/thumbnail.png"
 }
 
 export const battleship = {
     name: "Library",
     desc: "Battleship game, which allows the user to play battleship in their browser",
-    img: ""
+    img: "../battleship-2/thumbnail.png"
 }
+
+
+export let data = { odin_recipes, Dashboard, calculator, Eas, SignUP, rps, Library, TDList, TicTacToe, Weather_app, Restaurant, battleship }
+
+console.log(data)
