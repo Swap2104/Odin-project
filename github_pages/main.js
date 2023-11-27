df3900f4d9b141e5f49359a1d844a07adfb0870e
Swapnil <108745622+Swapnilnaique@users.noreply.github.com>
2023-11-28 01:43:10 +0530

    added recent peojects section

--- github_pages/main.js
+++ github_pages/main.js
@@ -8,31 +8,23 @@ import { data } from "./project_data.js"
 
 Navbar()
 
-let img = document.getElementsByClassName('img')
-let img_id = document.getElementById('img-id')
 
 const project_0 = document.getElementById('project0')
 const project_1 = document.getElementById('project1')
 const project_2 = document.getElementById('project2')
 const project_3 = document.getElementById('project3')
-console.log(data.TicTacToe.name)
-console.log(project_0)
 
 
+const head=document.getElementsByClassName('heading')
+const para=document.getElementsByClassName('para')
 
-project_0.textContent = data.TicTacToe.name
-project_1.textContent = data.Weather_app.name
-project_2.textContent = data.Restaurant.name
-project_3.textContent = data.battleship.name
+head[0].textContent=data.TicTacToe.name
+head[1].textContent=data.Weather_app.name
+head[2].textContent=data.Restaurant.name
+head[3].textContent=data.battleship.name
 
+para[0].textContent=data.TicTacToe.desc
+para[1].textContent=data.Weather_app.desc
+para[2].textContent=data.Restaurant.desc
+para[3].textContent=data.battleship.desc
 
-
-
-// for (let i = 0; i < data.length; i++) {
-//     console.log(data[i].img)
-//     console.log({ i })
-//     console.log(img[i])
-//     img[i].style.backgroundImage = "url(" + data[i].img + ")"
-//     // img[1].style.backgroundImage = "url(" + data[1].img + ")"
-// }
-// console.log(data.length)
