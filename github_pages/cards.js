39dc4d3f2cc6dea7f88844ad0d2d8687683bc7b9
Swapnil <108745622+Swapnilnaique@users.noreply.github.com>
2023-11-29 01:56:28 +0530

    added function to create cards

---
+++ github_pages/cards.js
@@ -0,0 +1,18 @@
+export function Cards() {
+    const project = document.createElement('div')
+    const heading = document.createElement('h1')
+    const para = document.createElement('para')
+    const link = document.createElement('a')
+    const content = document.getElementById("content")
+
+
+    heading.className = "heading"
+    para.classList.add("para")
+    link.setAttribute("target", "_blank")
+    link.textContent = ""
+
+    content.appendChild(project)
+    project.appendChild(heading)
+    project.appendChild(para)
+    project.appendChild(link)
+}
