e76e022729595e02f5bb0f2b7d6d01eb4fda2fbb
Swap <108745622+Swapnilnaique@users.noreply.github.com>
2023-12-01 01:03:44 +0530

    fixed project links

--- github_pages/project_data.js
+++ github_pages/project_data.js
@@ -11,73 +11,73 @@ export const data = [
     {
         name: "odin-recipes",
         desc: "A very simple recipes page only using html. It is the first project in the odin-project curriculum",
-        link: "odin-recipes"
+        link: "../odin-recipes"
     },
 
     {
         name: "Dashboard",
         desc: "Simple dashboard template",
-        link: "Dashboard"
+        link: "../Dashboard"
     },
 
     {
         name: "Calculator",
         desc: "Web based calculator project that performs simple arithmetic operations",
-        link: "Calculator"
+        link: "../Calculator"
     },
 
     {
         name: "Etch-a-sketch",
         desc: "Etch-a-sketch webpage built using javascript",
-        link: "Etch-a-sketch"
+        link: "../Etch-a-sketch"
     },
 
     {
         name: "Sign up page",
         desc: "Simple sign-up page with Password validation built using HTML, CSS and javascript",
-        link: "sign-up/"
+        link: "../sign-up/"
     },
 
     {
         name: "Rock Paper Scissors",
         desc: "Web based Rock paper Scissor game",
-        link: "RPS"
+        link: "../RPS"
     },
 
     {
         name: "Library",
         desc: "Web based personal library which allows the user to store the names of the books they have read and want to read",
-        link: "Library"
+        link: "../Library"
     },
 
     {
         name: "To-Do List",
         desc: "Web based To-Do list to monitor your daily tasks",
-        link: "to-do-list"
+        link: "../to-do-list/dist"
     },
 
     {
         name: "Tic Tac Toe",
         desc: "Play Tic-Tac-Toe with AI",
-        link: "Tic-Tac-Toe"
+        link: "../Tic-Tac-Toe"
     },
 
     {
         name: "Weather App",
         desc: "Web app to check weather around the world. This app was built using webpack",
-        link: "Weather-app"
+        link: "../Weather-app"
     },
 
     {
         name: "Restaurant Page",
         desc: "Working Restaurant page built using webpack",
-        link: "Restaurant-webpack/dist"
+        link: "../Restaurant-webpack/dist"
     },
 
     {
         name: "Battleship",
         desc: "Battleship game, which allows the user to play battleship in their browser",
-        link: "battleship-2/dist/"
+        link: "../battleship-2/dist/"
     }
 
 ]
