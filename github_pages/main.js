7fcd2b1bd1ebbeec2095893bf8000b04415a5c0d
Swapnil <108745622+Swapnilnaique@users.noreply.github.com>
2023-11-30 16:09:05 +0530

    added links to cards

--- github_pages/main.js
+++ github_pages/main.js
@@ -12,8 +12,10 @@ console.log(data)
 
 const head = document.getElementsByClassName('heading')
 const para = document.getElementsByClassName('para')
+const link = document.getElementsByClassName('link')
 
-for (let i = 1, j = 0; i <= 4; i++ , j++) {
+for (let i = 1, j = 0; i <= 4; i++, j++) {
     head[j].textContent = data[data.length - i].name
     para[j].textContent = data[data.length - i].desc
+    link[j].setAttribute("href", data[data.length - i].link)
 }
