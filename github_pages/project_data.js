b665d345ecd59cd5625b7203fb52000b7dbd2a20
Swapnil <108745622+Swapnilnaique@users.noreply.github.com>
2023-11-30 15:54:20 +0530

    added link property removed img

--- github_pages/project_data.js
+++ github_pages/project_data.js
@@ -11,73 +11,73 @@ export const data = [
     {
         name: "odin-recipes",
         desc: "A very simple recipes page only using html. It is the first project in the odin-project curriculum",
-        img: "../odin-recipes/thumbnail.png"
+        link: "odin-recipes"
     },
 
     {
         name: "Dashboard",
         desc: "Simple dashboard template",
-        img: "../Dashboard/thumbnail.png"
+        link: "Dashboard"
     },
 
     {
         name: "Calculator",
         desc: "Web based calculator project that performs simple arithmetic operations",
-        img: "../Calculator/thumbnail.png"
+        link: "Calculator"
     },
 
     {
         name: "Etch-a-sketch",
         desc: "Etch-a-sketch webpage built using javascript",
-        img: "../Etch-a-sketch/thumbnail.png"
+        link: "Etch-a-sketch"
     },
 
     {
         name: "Sign up page",
         desc: "Simple sign-up page with Password validation built using HTML, CSS and javascript",
-        img: "../sign-up/thumbnail.png"
+        link: "sign-up/"
     },
 
     {
         name: "Rock Paper Scissors",
         desc: "Web based Rock paper Scissor game",
-        img: "../RPS/thumbnail.png"
+        link: "RPS"
     },
 
     {
         name: "Library",
         desc: "Web based personal library which allows the user to store the names of the books they have read and want to read",
-        img: "../Library/thumbnail.png"
+        link: "Library"
     },
 
     {
         name: "To-Do List",
         desc: "Web based To-Do list to monitor your daily tasks",
-        img: "../to-do-list/thumbnail.png"
+        link: "to-do-list"
     },
 
     {
         name: "Tic Tac Toe",
         desc: "Play Tic-Tac-Toe with AI",
-        img: "../Tic-Tac-Toe/thumbnail.png"
+        link: "Tic-Tac-Toe"
     },
 
     {
         name: "Weather App",
         desc: "Web app to check weather around the world. This app was built using webpack",
-        img: "../Weather-app/thumbnail.png"
+        link: "Weather-app"
     },
 
     {
         name: "Restaurant Page",
         desc: "Working Restaurant page built using webpack",
-        img: "../Restaurant-webpack/thumbnail.png"
+        link: "Restaurant-webpack"
     },
 
     {
         name: "Battleship",
         desc: "Battleship game, which allows the user to play battleship in their browser",
-        img: "../battleship-2/thumbnail.png"
+        link: "battleship-2"
     }
 
 ]
