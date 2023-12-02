05fcd72e06e293f53ddc5b510e142b3972ae884f
Swap <108745622+Swapnilnaique@users.noreply.github.com>
2023-12-02 10:25:27 +0530

    fixed file paths to all the projects

--- github_pages/project_data.js
+++ github_pages/project_data.js
@@ -11,155 +11,75 @@ export const data = [
     {
         name: "odin-recipes",
         desc: "A very simple recipes page only using html. It is the first project in the odin-project curriculum",
-        link: "../odin-recipes"
+        link: "/odin-recipes"
     },
 
     {
         name: "Dashboard",
         desc: "Simple dashboard template",
-        link: "../Dashboard"
+        link: "/Dashboard"
     },
 
     {
         name: "Calculator",
         desc: "Web based calculator project that performs simple arithmetic operations",
-        link: "../Calculator"
+        link: "/Calculator"
     },
 
     {
         name: "Etch-a-sketch",
         desc: "Etch-a-sketch webpage built using javascript",
-        link: "../Etch-a-sketch"
+        link: "/Etch-a-sketch"
     },
 
     {
         name: "Sign up page",
         desc: "Simple sign-up page with Password validation built using HTML, CSS and javascript",
-        link: "../sign-up/"
+        link: "/sign-up/"
     },
 
     {
         name: "Rock Paper Scissors",
         desc: "Web based Rock paper Scissor game",
-        link: "../RPS"
+        link: "/RPS"
     },
 
     {
         name: "Library",
         desc: "Web based personal library which allows the user to store the names of the books they have read and want to read",
-        link: "../Library"
+        link: "/Library"
     },
 
     {
         name: "To-Do List",
         desc: "Web based To-Do list to monitor your daily tasks",
-        link: "../to-do-list/dist"
+        link: "/to-do-list/dist"
     },
 
     {
         name: "Tic Tac Toe",
         desc: "Play Tic-Tac-Toe with AI",
-        link: "../Tic-Tac-Toe"
+        link: "/Tic-Tac-Toe"
     },
 
     {
         name: "Weather App",
         desc: "Web app to check weather around the world. This app was built using webpack",
-        link: "../Weather-app"
+        link: "/Weather-app"
     },
 
     {
         name: "Restaurant Page",
         desc: "Working Restaurant page built using webpack",
-        link: "../Restaurant-webpack/dist"
+        link: "/Restaurant-webpack/dist"
     },
 
     {
         name: "Battleship",
         desc: "Battleship game, which allows the user to play battleship in their browser",
-        link: "../battleship-2/dist/"
+        link: "/battleship-2/dist/"
     }
 
 ]
-
-
-
-
-// export const odin_recipes = {
-//     name: "odin-recipes",
-//     desc: "A very simple recipes page only using html. It is the first project in the odin-project curriculum",
-//     img: "../odin-recipes/thumbnail.png"
-// }
-
-// export const Dashboard = {
-//     name: "Dashboard",
-//     desc: "Simple dashboard template",
-//     img: "../Dashboard/thumbnail.png"
-// }
-
-// export const calculator = {
-//     name: "Calculator",
-//     desc: "Web based calculator project that performs simple arithmetic operations",
-//     img: "../Calculator/thumbnail.png"
-// }
-
-// export const Eas = {
-//     name: "Etch-a-sketch",
-//     desc: "Etch-a-sketch webpage built using javascript",
-//     img: "../Etch-a-sketch/thumbnail.png"
-// }
-
-// export const SignUP = {
-//     name: "Sign up page",
-//     desc: "Simple sign-up page with Password validation built using HTML, CSS and javascript",
-//     img: "../sign-up/thumbnail.png"
-// }
-
-// export const rps = {
-//     name: "Rock Paper Scissors",
-//     desc: "Web based Rock paper Scissor game",
-//     img: "../RPS/thumbnail.png"
-// }
-
-// export const Library = {
-//     name: "Library",
-//     desc: "Web based personal library which allows the user to store the names of the books they have read and want to read",
-//     img: "../Library/thumbnail.png"
-// }
-
-// export const TDList = {
-//     name: "To-Do List",
-//     desc: "Web based To-Do list to monitor your daily tasks",
-//     img: "../to-do-list/thumbnail.png"
-// }
-
-// export const TicTacToe = {
-//     name: "Tic Tac Toe",
-//     desc: "Play Tic-Tac-Toe with AI",
-//     img: "../Tic-Tac-Toe/thumbnail.png"
-// }
-
-// export const Weather_app = {
-//     name: "Weather App",
-//     desc: "Web app to check weather around the world. This app was built using webpack",
-//     img: "../Weather-app/thumbnail.png"
-// }
-
-// export const Restaurant = {
-//     name: "Restaurant Page",
-//     desc: "Working Restaurant page built using webpack",
-//     img: "../Restaurant-webpack/thumbnail.png"
-// }
-
-// export const battleship = {
-//     name: "Battleship",
-//     desc: "Battleship game, which allows the user to play battleship in their browser",
-//     img: "../battleship-2/thumbnail.png"
-// }
-
-
-
-
-// export let data = { odin_recipes, Dashboard, calculator, Eas, SignUP, rps, Library, TDList, TicTacToe, Weather_app, Restaurant, battleship }
 
 console.log(data)
