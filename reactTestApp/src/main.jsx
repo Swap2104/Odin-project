98f90a777f4def6dd25cc36c9dd0bda5699de3d4
Swap <108745622+Swapnilnaique@users.noreply.github.com>
2023-12-10 23:09:07 +0530

    added new component

--- reactTestApp/src/main.jsx
+++ reactTestApp/src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Hello from './Hello.jsx'
+import Comp from './newcomp.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Hello />
+    <Comp />
+    {/* <Hello /> */}
     {/* <App /> */}
   </React.StrictMode>,
 )
---
+++ reactTestApp/src/newcomp.jsx
@@ -0,0 +1,20 @@
+function Comp(params) {
+  return (
+    <>
+      <h1>Test title</h1>
+      <ol className="test-list">
+        <li>List item 1</li>
+        <li>List item 2</li>
+        <li>List item 3</li>
+      </ol>
+      <svg>
+        <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+      </svg>
+      <form>
+        <input type="text" />
+      </form>
+    </>
+  );
+}
+
+export default Comp;
