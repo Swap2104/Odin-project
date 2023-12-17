03def4460bfd478427a798625b1e587fbce917cb
Swap <108745622+Swapnilnaique@users.noreply.github.com>
2023-12-18 01:59:25 +0530

    added InputSection

--- cvBuilder/src/components/InputSection.jsx
+++ cvBuilder/src/components/InputSection.jsx
@@ -1,7 +1,20 @@
+import Buttons from "./Buttons";
+import "../styles/InputSection.css";
+
 const InputSection = () => {
   return (
-    <div>
-      <h1>This is the Input section</h1>
+    <div className="InputSection">
+      <div>
+        {/* Create three components for the three section */}
+        {/* Increment/decrement a value using the back and next */}
+        {/* Based on the value change the component */}
+        
+      </div>
+      <div>
+      {/* This div contains the buttons. Pass the clickFunction */}
+        <Buttons text={"Next"} />
+        <Buttons text={"Back"} />
+      </div>
     </div>
   );
 };
