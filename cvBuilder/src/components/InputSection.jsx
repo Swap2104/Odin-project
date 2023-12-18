34586105123e427939cac2e86b1902be920a7e34
Swap <108745622+Swapnilnaique@users.noreply.github.com>
2023-12-18 20:11:59 +0530

    added PersonalInfo form

--- cvBuilder/src/components/InputSection.jsx
+++ cvBuilder/src/components/InputSection.jsx
@@ -1,5 +1,6 @@
 import Buttons from "./Buttons";
 import "../styles/InputSection.css";
+import PersonalInfo from "./PersonalInfo";
 
 const InputSection = () => {
   return (
@@ -8,7 +9,7 @@ const InputSection = () => {
         {/* Create three components for the three section */}
         {/* Increment/decrement a value using the back and next */}
         {/* Based on the value change the component */}
-        
+        <PersonalInfo />
       </div>
       <div>
       {/* This div contains the buttons. Pass the clickFunction */}
---
+++ cvBuilder/src/components/PersonalInfo.jsx
@@ -0,0 +1,22 @@
+import '../styles/PersonalInfo.css'
+
+const PersonalInfo = () => {
+    return (
+      <form action="" className="PersonalInfo">
+
+        <label htmlFor="firstName">First Name</label>
+        <label htmlFor="lastName">Last Name</label>
+
+        <input type="text" name="firstName" placeholder='FirstName'/>
+        <input type="text" name="lastName" placeholder='LastName'/>
+
+        <label htmlFor="phone">Phone</label>
+        <label htmlFor="email">Email</label>
+
+        <input type="text" name="phone" placeholder='Phone'/>
+        <input type="text" name="email" placeholder='Email'/>
+      </form>
+    );
+}
+ 
+export default PersonalInfo;
