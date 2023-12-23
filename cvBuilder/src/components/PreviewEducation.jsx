8d7539af4ae64f72f544f13a06ee7a1ce74f7a67
Swap <108745622+Swapnilnaique@users.noreply.github.com>
2023-12-23 20:21:14 +0530

    added education and experience component

--- cvBuilder/src/components/PreviewEducation.jsx
+++ cvBuilder/src/components/PreviewEducation.jsx
@@ -1,10 +1,23 @@
+import PreviewCard from "./PreviewCard";
+
 const PreviewEducation = () => {
-    // ? Education section in the Preview
+  // ? Education section in the Preview
   return (
     <div id="Education">
       <h2>Education</h2>
+          {/* create a new component that insets a div with the name of the institute, Degree, start and end date  */}
       <div>
-        {/* create a new component that insets a div with the name of the institute, Degree, start and end date  */}
+        <PreviewCard
+          institute={"GPC"}
+          degree={"Computer Engineering"}
+          StartEnd={"2021-2024"}
+        />
+
+        <PreviewCard
+          institute={"GPC"}
+          degree={"Computer Engineering"}
+          StartEnd={"2021-2024"}
+        />
       </div>
     </div>
   );
--- cvBuilder/src/components/PreviewExperience.jsx
+++ cvBuilder/src/components/PreviewExperience.jsx
@@ -1,13 +1,19 @@
+import PreviewCard from "./PreviewCard";
+
 const PreviewExperience = () => {
   // ? Work experience section in the Preview
   return (
     <div id="Experience">
       <h2>Work Experience</h2>
       <div>
-        
+        <PreviewCard
+          institute={"GPC"}
+          degree={"Computer Engineering"}
+          StartEnd={"2021-2024"}
+        />
       </div>
     </div>
   );
-}
- 
+};
+
 export default PreviewExperience;
