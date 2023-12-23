a0f773bd7c546d08111f8d5d8c25a8f3e378d0f8
Swap <108745622+Swapnilnaique@users.noreply.github.com>
2023-12-23 17:56:53 +0530

    experience and education section

--- cvBuilder/src/components/PreviewEducation.jsx
+++ cvBuilder/src/components/PreviewEducation.jsx
@@ -1,7 +1,11 @@
 const PreviewEducation = () => {
+    // ? Education section in the Preview
   return (
-    <div id="Experience">
-      <h2>Work Experience</h2>
+    <div id="Education">
+      <h2>Education</h2>
+      <div>
+        {/* create a new component that insets a div with the name of the institute, Degree, start and end date  */}
+      </div>
     </div>
   );
 };
--- cvBuilder/src/components/PreviewExperience.jsx
+++ cvBuilder/src/components/PreviewExperience.jsx
@@ -1,9 +1,13 @@
 const PreviewExperience = () => {
-    return ( 
-        <div id="Experience" >
-            <h2>Work Experience</h2>
-        </div>
-     );
+  // ? Work experience section in the Preview
+  return (
+    <div id="Experience">
+      <h2>Work Experience</h2>
+      <div>
+        
+      </div>
+    </div>
+  );
 }
  
 export default PreviewExperience;
